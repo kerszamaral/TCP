69fdb2ff52ec3ffa00cf42cef46e178c40bc428d
Ian Kersz <kerszamaral@gmail.com>
2023-05-23 09:50:38 -0300

    Setup enviroment

---
+++ src/lab00/AppTest.java
@@ -0,0 +1,12 @@
+package lab00;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class AppTest {
+
+    @Test
+    public void test() {
+        Assert.assertEquals(2, 1 + 1);
+    }
+}
---
+++ src/lab00/hello.java
@@ -0,0 +1,7 @@
+package lab00;
+
+class helloword {
+    public static void main(String[] args) {
+        System.out.println("Current JVM version - " + System.getProperty("java.version"));
+    }
+}
