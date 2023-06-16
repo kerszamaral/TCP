671117585436eae897015c596db6d5e5d114511d
Ian Kersz <kerszamaral@gmail.com>
2023-06-16 20:15:59 -0300

    Finished Professor class

--- src/lab03/universidade/Professor.java
+++ src/lab03/universidade/Professor.java
@@ -1,5 +1,58 @@
 package lab03.universidade;
 
 public class Professor {
-    
+    private int id;
+
+    private String departamento;
+
+    /**
+     * Default constructor
+     */
+    public Professor() {
+        id = -1; // Assumimos que o id é -1
+        departamento = ""; // Assumimos que o departamento é vazio
+    }
+
+    /**
+     * @param id
+     * @param departamento
+     */
+    public Professor(int id, String departamento) {
+        if (id < 0) {
+            throw new IllegalArgumentException("Id deve ser maior ou igual a 0");
+        }
+        this.id = id;
+        this.departamento = departamento;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        if (id < 0) {
+            throw new IllegalArgumentException("Id deve ser maior ou igual a 0");
+        }
+        this.id = id;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @return the departamento
+     */
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    /**
+     * @param departamento the departamento to set
+     */
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
 }
